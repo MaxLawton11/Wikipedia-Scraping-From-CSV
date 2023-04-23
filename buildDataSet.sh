97d86618eddfb7c034c67307006eb0ede84e0f82
Max Lawton <max@lawtonnation.com>
2023-04-22 23:46:22 -0400

    added shell

---
+++ buildDataSet.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+pip3 install pandas
+pip3 install mwparserfromhell
+pip3 install requests
+
+CYAN= '\033[0;36m'
+WHITE= '\033[0;37m' 
+RED= '\033[0;31m'   
+echo -e "${Blue}Welcome ${WHITE}to ${RED}France"
+
+echo -e "${White}Author  :  ${Cyan}Max Lawton${White}"
+echo -e "${White}Version :  ${Cyan}Apr 22, 2023${White}"
