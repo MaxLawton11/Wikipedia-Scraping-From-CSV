345ab40116c52d40a422e71d6e2350a2f6c7dc33
Max Lawton <max@lawtonnation.com>
2023-05-16 21:45:58 -0400

    changed name

--- buildDataSet.sh
+++ buildDataSet.sh
@@ -4,20 +4,20 @@ CYAN= '\033[0;36m'
 WHITE= '\033[0;37m' 
 GREEN= '\033[0;32m'   
 
-echo -e "${White}Author  :  ${Cyan}Max Lawton${White}"
-echo -e "${White}Version :  ${Cyan}Apr 22, 2023${White}"
+echo -e "# ${White}Author  :  ${Cyan}Max Lawton${White}"
+echo -e "# ${White}Version :  ${Cyan}Apr 22, 2023${White}"
 
 pip3 install pandas
 pip3 install mwparserfromhell
 pip3 install requests
 
-echo -e "${Green}Added all Required Dependencys${White}"
-echo -e "${Green}Compiling dataset:${White}"
+echo -e "# ${Green}Added all Required Dependencys${White}"
+echo -e "# ${Green}Compiling dataset:${White}"
 
 python3 Wikipedia_CSVs.py
 
-echo -e "${Green}Setting up dataset segmentation:${White}"
+echo -e "# ${Green}Setting up dataset segmentation:${White}"
 
 python3 BreakDown.py
 
-echo -e "${Green}Finished!${White}"
+echo -e "# ${Green}Finished!${White}"
